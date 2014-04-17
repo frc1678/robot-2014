bed7c6e29a4848943e9c4d0bf51e16bacd7fca15
unknown <CitrusCircuits@gmail.com>
2014-04-17 15:21:32 -0700

    Changed manipulator buttons for EtekCity controller

--- Definitions.h
+++ Definitions.h
@@ -8,20 +8,20 @@
 #define k_bshotAlignLong driverL, 1
 #define k_bshotAlignShort driverR, 1
 //Intakes
-#define k_bfrontIntakePickup manipulator, 8
-#define k_bbackIntakePickup manipulator, 6
-#define k_bfrontIntakeDeployToggle manipulator, 1
-#define k_bbackIntakeDeployToggle manipulator, 2
-#define k_breverseIntake manipulator, 3
-#define k_bhumanLoad manipulator, 4
+#define k_bfrontIntakePickup manipulator, 6
+#define k_bbackIntakePickup manipulator, 8
+#define k_bfrontIntakeDeployToggle manipulator, 4
+#define k_bbackIntakeDeployToggle manipulator, 3
+#define k_breverseIntake manipulator, 2
+#define k_bhumanLoad manipulator, 1
 #define k_brunSecondary manipulator, 9
 
 //shooter
 #define k_bshortShoot driverR, 1
 #define k_blongShoot driverL, 1
 #define k_bshooterPrime manipulator, 11
-#define k_bpulseSecondary manipulator, 5
-#define k_bfoldFlower manipulator, 7
+#define k_bpulseSecondary manipulator, 7
+#define k_bfoldFlower manipulator, 5
 #define k_barmPistonToggle manipulator, 10
 #define k_bkillShotL driverL, 3
 #define k_bkillShotR driverR, 3
