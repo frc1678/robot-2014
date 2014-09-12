596b0b8c5330cf3790aa982984264c70bf803121
LilTiger <tahoekeo@gmail.com>
2014-09-12 15:02:39 -0700

    Updated after Chezy Champs and the summer

--- Definitions.h
+++ Definitions.h
@@ -8,24 +8,25 @@
 #define k_bshotAlignLong driverL, 1
 #define k_bshotAlignShort driverR, 1
 //Intakes
-#define k_bfrontIntakePickup manipulator, 6
-#define k_bbackIntakePickup manipulator, 8
-#define k_bfrontIntakeDeployToggle manipulator, 4
-#define k_bbackIntakeDeployToggle manipulator, 3
-#define k_breverseIntake manipulator, 2
+#define k_bfrontIntakePickup manipulator, 5
+#define k_bbackIntakePickup manipulator, 6
+#define k_bfrontIntakeDeployToggle manipulator, 3
+#define k_bbackIntakeDeployToggle manipulator, 2
+#define k_breverseIntake manipulator, 4
 #define k_bhumanLoad manipulator, 1
 #define k_brunSecondary manipulator, 9
 
 //shooter
 #define k_bshortShoot driverR, 1
 #define k_blongShoot driverL, 1
-#define k_bshooterPrime manipulator, 11
+#define k_bshooterPrime manipulator, 12
 #define k_bpulseSecondary manipulator, 7
-#define k_bfoldFlower manipulator, 5
+#define k_bfoldFlower manipulator, 8
 #define k_barmPistonToggle manipulator, 10
 #define k_bkillShotL driverL, 3
 #define k_bkillShotR driverR, 3
 #define k_btoggleShotAlign manipulator, 7
+#define k_breverseHumanLoad manipulator, 11
 		
 #endif //DEFINITIONS_H
 
